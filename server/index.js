3b455d50c6b8daad518695ac4f9a44cfe74e9977
wbc277072080 <bochaowang1997@gmail.com>
2020-10-29 10:16:42 -0400

    first upload

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.get('/',(req,res)=>{
+app.get('/greeting',(req,res)=>{
     res.send({hi:'there'});
 });
 
-app.listen(5000);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
